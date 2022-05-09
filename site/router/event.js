26a4ab4174b1603a8c479eef9bd70e6a08e2a1d7
Thibault Rucher <thibault.rucher@etu.univ-poitiers.fr>
2022-05-09 16:28:22 +0200

    events routes

--- site/router/event.js
+++ site/router/event.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-let events = [];
+let events = [
+    {
+        id: 1,
+        title: "Event 1",
+        date: "2020-01-01",
+    },
+    {
+        id: 2,
+        title: "Event 2",
+        date: "2020-01-02",
+    },
+    {
+        id: 3,
+        title: "Event 3",
+        date: "2020-01-03",
+    }
+];
 
 router.get("/", (req, res) => {
-   res.send({
-      message: "Welcome to the event router"
-   })
+   res.send(events)
+});
+
+router.get("/:id", (req, res) => {
+    let ev = events.filter(event => event.id === req.params.id);
+    res.send(ev);
 });
 
 module.exports = {
