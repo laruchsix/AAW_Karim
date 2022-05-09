34f539129dfdcd0fa846bfc7891eb6225767d87b
Tiwan17 <lenglet.jolan17@hotmail.com>
2022-05-09 17:45:46 +0200

    html page

--- site/src/index.jsx
+++ site/src/index.jsx
@@ -1,4 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1>Coucou</h1>, document.getElementById("root"))
+const elem = <h1>hello you</h1>
+
+function Welcome(props){
+    return <h1>Bonjour, {props.name}</h1>
+}
+
+class WelcomeClass extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {date:new Date()}
+    }
+
+    render() {
+        return <h1>Bonjour, {this.props.name}, il est {this.state.date.toLocaleTimeString()}</h1>
+    }
+}
+
+function App(props){
+    return (
+        <div>
+            <WelcomeClass name="Bibo" />
+            <WelcomeClass name="Mousse"/>
+            <WelcomeClass name="Jo"/>
+        </div>
+    )
+}
+
+Class
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
