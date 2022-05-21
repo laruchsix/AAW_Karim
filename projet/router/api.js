39c8b27497b5a7a0a92bfb6b37d22808a5bf5e56
Thibault Rucher <thibault.rucher@etu.univ-poitiers.fr>
2022-05-21 13:21:50 +0200

    utils + databaseRequest file

--- projet/router/api.js
+++ projet/router/api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const personRouter = require("./person");
+const planningRouter = require("./planning");
 
 router.use("/person", personRouter);
+router.use("/planning", planningRouter);
 
 module.exports = router;
 
