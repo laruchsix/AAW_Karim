6a21d3498d496a2735ca6964bcf79633ecf03534
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2022-05-24 11:35:23 +0200

    planning start

--- projet/src/app/index.jsx
+++ projet/src/app/index.jsx
@@ -6,12 +6,39 @@ import "./style/index.css"
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            plannings:[],
+            id:"Unknown ID",
+            name:"Unknown name",
+            date:"Unknown date"
+        }
     }
 
     render() {
         return (
             <div className="app-container">
                 <Menu />
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.plannings.map((planning) => {
+                            return <tr>
+                                <td>{planning.id}</td>
+                                <td>{planning.name}</td>
+                                <td>{planning.date}</td>
+                            </tr>
+                        })
+                    }
+                    </tbody>
+                </table>
             </div>
         );
     }
