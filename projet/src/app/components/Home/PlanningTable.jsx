9dd209b52f32c6116b2552b55d01c0a93da58b2f
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2022-05-24 21:13:30 +0200

    planning add Test

--- projet/src/app/components/Home/PlanningTable.jsx
+++ projet/src/app/components/Home/PlanningTable.jsx
@@ -1,36 +1,21 @@
 import React, {useState} from "react";
 import "../../style/planningTable.css"
 
-const PlanningAdd = (props)=>{
-    const [name, setName] = useState("");
-    const [date, setDate] = useState("");
-
-    const addPlanning = (e) => {
-        e.preventDefault();
-        const body = JSON.stringify({"name":name, "date" : date});
-        fetch('api/planning', {
-            method: "POST",
-            body: body,
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
-        })
-            .then((response) => response.json())
-            .then((events)=> {
-                props.onAdd();
-            });
-    }
-
-    return (
-        <div className={"planning-add"}>
-            <form onSubmit={(e) => addPlanning(e)}>
-                <input type={"text"} value = {name} onChange={(e)=>setName(e.currentTarget.value)} />
-                <input type={"text"} value = {date} onChange={(e)=>setDate(e.currentTarget.value)} />
-                <button>Ajouter</button>
-            </form>
-        </div>
-    );
+const addPlanning = (e) => {
+    e.preventDefault();
+    const body = JSON.stringify({"name":name, "date" : date});
+    fetch('api/planning', {
+        method: "POST",
+        body: body,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then((events)=> {
+            loadPlannings();
+        });
 }
 
 const loadPlannings = () => {
@@ -52,6 +37,8 @@ const loadPlannings = () => {
 
 const PlanningTable = ({updateSelectedPlanning}) => {
     const [planning, setPlanning] = useState();
+    const [name, setName] = useState("");
+    const [date, setDate] = useState("");
 
     // TODO : the planning request
     if (planning === undefined) {
@@ -72,33 +59,38 @@ const PlanningTable = ({updateSelectedPlanning}) => {
         <div className={"planning-container"}>
             <h1>Planning</h1>
             {
-                (planning === undefined || planning.loading === true) ?
-                    <p>Loading...</p>
-                    : <div className={"planning-table"}>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>date</th>
-                                <th>view</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-                                planning.data.map((p) => {
-                                    return <tr key={p.id}>
-                                        <td>{p.name}</td>
-                                        <td>{p.date}</td>
-                                        <td><button onClick={() => updateSelectedPlanning(p)}>View</button></td>
-                                    </tr>
-                                })
-                            }
-                            </tbody>
-                        </table>
-                    </div>
+            (planning === undefined || planning.loading === true) ?
+                <p>Loading...</p>
+                : <div className={"planning-table"}>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>date</th>
+                            <th>view</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            planning.data.map((p) => {
+                                return <tr key={p.id}>
+                                    <td>{p.name}</td>
+                                    <td>{p.date}</td>
+                                    <td><button onClick={() => updateSelectedPlanning(p)}>View</button></td>
+                                </tr>
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </div>
             }
-
-            <PlanningAdd onAdd={() => loadPlannings()}/>
+            <div className={"planning-add"}>
+                <form onSubmit={(e) => addPlanning(e)}>
+                    <input type={"text"} value = {name} onChange={(e)=>setName(e.currentTarget.value)} />
+                    <input type={"text"} value = {date} onChange={(e)=>setDate(e.currentTarget.value)} />
+                    <button>Ajouter</button>
+                </form>
+            </div>
         </div>
     )
 }
