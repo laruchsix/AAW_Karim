dc47fba6c65397f091724d74d8bba91d51bbb290
Rémi Orveau <78747414+Bleomoon@users.noreply.github.com>
2022-05-24 11:42:22 +0200

    planning 1

---
+++ site/router/manche.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const requestManager = require("../database/databaseRequest");
+
+
+router.get("/:id", (req, res) => {
+    if (req.params.id === undefined) {
+        res.status(400).send({
+            error: "require the planning id"
+        });
+    }
+
+    let sqlRequest = {
+        name: "read-manche-" + req.params.id,
+        text: 'SELECT * FROM manche WHERE planning_id = ($1) ORDER BY ordre;',
+        values: [req.params.id]
+    }
+
+    requestManager.basicRequest(sqlRequest, res);
+});
+
+module.exports = router;
