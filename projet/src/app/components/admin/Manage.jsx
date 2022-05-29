80e23ba953c2255b8925cd5837b2bb72d621ad1d
Thibault Rucher <thibault.rucher@etu.univ-poitiers.fr>
2022-05-29 15:35:57 +0200

    style home page

--- projet/src/app/components/admin/Manage.jsx
+++ projet/src/app/components/admin/Manage.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
-import "../../style/Manage.css";
 
 const Manage = ({token}) => {
     const history = useHistory();
